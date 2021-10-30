4870e60d5710467599fa32e2ca4270ea7bbbfcd2
Genne23v <zakk23v@hotmail.com>
2021-10-30 17:33:13 -0400

    Refectoring test files. Add Ruby test files

--- Form.java
+++ Form.java
@@ -11,16 +11,13 @@ public class Form {
 
 		driver.get("https://formy-project.herokuapp.com/form");
 
-		driver.findElement(By.id("first-name")).sendKeys("John");
-		driver.findElement(By.id("last-name")).sendKeys("Doe");
-		driver.findElmenet(By.id("job-title")).sendKeys("QA Engineer");
+		FormPage formPage = new FormPage();
+		formPage.submitForm(driver);
 
-		driver.findElement(By.id("radio-button-2")).click();
-		driver.findElement(By.id("checkbox-2")).click();
-		driver.findElement(By.cssSelector("option[value='1']").click();
-		driver.findElement(By.id("datepicker")).sendKeys("05/28/2019");
-		driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN); 
-		driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+		ConfirmationPage confirmationPage = new ConfirmationPage();
+		confirmationPage.waitForAlertBanner(driver);
+
+		assertEquals("The form was successfully submitted!", confirmationPage.getAlertBannerText(driver));
 
 		driver.quit();
 	}
