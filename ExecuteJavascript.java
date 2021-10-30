1cd0498e1fff8b7a4921b990f760c36394d8210a
Genne23v <zakk23v@hotmail.com>
2021-10-30 14:51:30 -0400

    modal test with JavascriptExecutor

---
+++ ExecuteJavascript.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class ExecuteJavascript{
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "Users/zakk2/Downloads/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://formy-project.herokuapp.com/modal");
+
+		WebElement modalButton = driver.findElement(By.id("modal-button"));
+		modalButton.click();
+
+		WebElement closeButton = driver.findElement(By.id("close-button"));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", closeButton);
+
+		driver.quit();
+	}
+}
